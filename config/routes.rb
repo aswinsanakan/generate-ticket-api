32003f106dc1ffbde26175e6f967beaac3c3abbb
aswinsanakan <aswinsanakan@gmail.com>
2017-01-11 13:40:43 +0530

    add pg for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :bookings
   end
+
+  root 'api/bookings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
